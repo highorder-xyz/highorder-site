96a8be09486484eb1c890c6c0426b68514a8997c
Wei Zhuo <zeaphoo@qq.com>
2023-08-19 10:10:37 +0800

    change theme to custom

--- site/.vitepress/config.ts
+++ site/.vitepress/config.ts
@@ -5,24 +5,5 @@ export default defineConfig({
   title: "HighOrder 高阶开发",
   description: "HighOrder",
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
   }
 })
---
+++ site/.vitepress/theme/Layout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useData } from 'vitepress'
+
+// https://vitepress.dev/reference/runtime-api#usedata
+const { frontmatter } = useData()
+</script>
+
+<template>
+  <div v-if="frontmatter.type == 'page'">
+    <h1> hola page </h1>
+  </div>
+  <div v-else>
+    <a href="/">Home</a>
+    <Content />
+  </div>
+</template>
