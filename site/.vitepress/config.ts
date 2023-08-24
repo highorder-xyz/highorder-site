132df907586c0caa757b7d797925562407246b0e
Wei Zhuo <zeaphoo@qq.com>
2023-08-24 19:49:06 +0800

    build works

--- site/.vitepress/config.ts
+++ site/.vitepress/config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "HighOrder 高阶开发",
-  description: "HighOrder",
-  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
-  themeConfig: {
-  }
+    title: "HighOrder 高阶开发",
+    description: "HighOrder",
+    head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+    themeConfig: {
+    },
+    vite: {
+        ssr: {
+            noExternal: ["primevue"]
+        }
+    }
 })
